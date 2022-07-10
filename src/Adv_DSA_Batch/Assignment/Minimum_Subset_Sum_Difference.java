74e0f2e756cf790920334881ef1bd9068a6732c1
Ankit kumar <ankitraaz0170@gnail.com>
2022-07-10 21:30:05 +0530

    initial commit

--- src/Adv_DSA_Batch/Assignment/Minimum_Subset_Sum_Difference.java
+++ src/Adv_DSA_Batch/Assignment/Minimum_Subset_Sum_Difference.java
@@ -1,4 +1,44 @@
 package Adv_DSA_Batch.Assignment;
 
 public class Minimum_Subset_Sum_Difference {
+    static int sum = 0;
+    public static void main(String[] args) {
+        int[] arr = {3, 3, 3, 3};
+        int n = arr.length;
+        int w = 6;
+        for (int i = 0; i < n; i++) {
+            sum+=arr[i];
+        }
+        System.out.println(KnapSack(arr, n, w));
+
+    }
+
+    static int KnapSack(int[] arr, int n, int w) {
+        boolean[][] dp = new boolean[n + 1][w + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < w + 1; j++) {
+                if (i == 0) dp[i][j] = false;
+                if (j == 0) dp[i][j] = true;
+            }
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < w + 1; j++) {
+                if (arr[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for (int k = 0; k < sum/2 ; k++) {
+            int first = k;
+            int last  = sum-k;
+            if(dp[n][k]==true && min> Math.abs(first-last))
+            {
+                min = Math.abs(first-last);
+            }
+        }
+        return min;
+    }
 }
